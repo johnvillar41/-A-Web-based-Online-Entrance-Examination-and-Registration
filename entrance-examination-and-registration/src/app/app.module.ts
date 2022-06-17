670f19c560cd02758f6fcc8e9413ed430009e14a
johnvillar41 <johndanielvillar123@gmail.com>
2022-06-17 19:48:41 +0800

    Added navs and showcase components

--- entrance-examination-and-registration/src/app/app.module.ts
+++ entrance-examination-and-registration/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { ShowcaseComponent } from './showcase/showcase.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ShowcaseComponent
   ],
   imports: [
     BrowserModule,

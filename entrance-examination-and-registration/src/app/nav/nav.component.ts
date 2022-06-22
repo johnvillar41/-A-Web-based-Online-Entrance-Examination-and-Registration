cdb470538c9baf61826d856146a5b78065d5abcd
johnvillar41 <johndanielvillar123@gmail.com>
2022-06-22 09:39:06 +0800

    added shared service

--- entrance-examination-and-registration/src/app/nav/nav.component.ts
+++ entrance-examination-and-registration/src/app/nav/nav.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthGuard } from 'src/auth.guard';
 import { AuthService } from 'src/services/auth-service.service';
+import { SharedService } from 'src/services/shared.service';
 
 @Component({
   selector: 'app-nav',
@@ -12,11 +13,18 @@ export class NavComponent implements OnInit {
   isLoggedIn: boolean = false;
   constructor(
     private auth: AuthService,
-    private route: ActivatedRoute,
-    private router: Router) { }
+    private sharedService: SharedService
+    ) { }
 
   ngOnInit(): void {
     this.isLoggedIn = this.auth.isAuthenticated();
+    this.sharedService.loginStatusMessage$.subscribe(status => {
+      if(status === true) {
+        this.isLoggedIn = true;
+      } else {
+        this.isLoggedIn = false;
+      }
+    });
   }
 
   onLogout(): void {
--- entrance-examination-and-registration/src/services/auth-service.service.ts
+++ entrance-examination-and-registration/src/services/auth-service.service.ts
@@ -1,34 +1,40 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { SharedService } from './shared.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   constructor(
-    private router: Router
+    private router: Router,
+    private sharedService: SharedService
   ) { }
 
   login(username: string, password: string): boolean {
-    //Fake login
     if (username == 'johny' && password == 'johny') {
       this.router.navigate(['/dashboard/home']);
       localStorage.setItem('token', "HAHAHAHAHATDOG");
+      this.sharedService.updateLoginStatus(true);
       return true;
     } else {
+      this.sharedService.updateLoginStatus(false);
       return false;
     }
   }
 
   logout(): void {
     localStorage.removeItem('token');
+    this.sharedService.updateLoginStatus(false);
   }
 
   isAuthenticated(): boolean {
     const token = localStorage.getItem('token');
     if (token == null) {
+      this.sharedService.updateLoginStatus(false);
       return false;
     }
+    this.sharedService.updateLoginStatus(true);
     return true;
   }
 }
